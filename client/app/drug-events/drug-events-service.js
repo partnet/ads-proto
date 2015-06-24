ebb4b2dbbeace15f3c7e06dc4c518dc46be147b7
Jeff Shipley <jshipley@part.net>
2015-06-24 14:13:04 -0600

    fix unit tests for pool 2 API, and deleted dead service code

--- client/app/drug-events/drug-events-service.js
+++ client/app/drug-events/drug-events-service.js
@@ -17,72 +17,8 @@
     .factory('DrugEventsService', ['$q', '$resource', function ($q, $resource) {
       var restServerURI = 'http://starbuck.part.net:8880/rest-api/faers/drugs/:drug';
 
-      var doReactionOutcomeRequest = function (deferred, outcomeQuery, index) {
-        var thisQuery = angular.copy(outcomeQuery);
-
-        thisQuery.search += '+AND+patient.reaction.reactionmeddrapt:"' + drugEventsFact.reactionResults[index].term + '"';
-        delete thisQuery.limit;
-
-        DrugEvents.get(angular.copy(thisQuery)).$promise.then(function (response) {
-          drugEventsFact.reactionResults[index].outcomes = response.results;
-
-          var outcomes = 0;
-          Object.keys(response.results).map(function (key) {
-            outcomes += response.results[key].count;
-          });
-
-          if (outcomes < drugEventsFact.reactionResults[index].count) {
-            var unknown = drugEventsFact.reactionResults[index].outcomes.filter(function (value) {
-              return value.term === 6;
-            });
-
-            if (unknown.length === 1) {
-              unknown[0].count += drugEventsFact.reactionResults[index].count - outcomes;
-            } else {
-              drugEventsFact.reactionResults[index].outcomes.push({
-                term: 6,
-                count: drugEventsFact.reactionResults[index].count
-              });
-            }
-          }
-          index++;
-          if (index < drugEventsFact.reactionResults.length) {
-            doReactionOutcomeRequest(deferred, outcomeQuery, index);
-          } else {
-            deferred.resolve();
-          }
-        }, function (response) {
-          switch (response.status) {
-            case 404:
-              drugEventsFact.reactionResults[index].outcomes = [{
-                term: 6,
-                count: drugEventsFact.reactionResults[index].count
-              }];
-          }
-          index++;
-          if (index < drugEventsFact.reactionResults.length) {
-            doReactionOutcomeRequest(deferred, outcomeQuery, index);
-          } else {
-            deferred.resolve();
-          }
-        });
-      };
-
-      var serializer = function (params) {
-        var ps = '';
-        var keys = Object.keys(params);
-        var count = 0;
-        keys.map(function (key) {
-          ps += key + '=' + params[key];
-          if (++count < keys.length) {
-            ps += '&';
-          }
-        });
-        return ps;
-      };
-
       var DrugEvents = $resource(restServerURI+"/reactions", {drug: '@drug'});
-      
+
       var Indications = $resource(restServerURI, {drug: '@drug'});
 
       var drugEventsFact = {
@@ -117,7 +53,7 @@
           drugEventsFact.reactionResults = response.reactions;
         });
       };
-      
+
       drugEventsFact.searchIndications = function (query) {
           console.log('Query: ' + JSON.stringify(query));
           return Indications.get(query).$promise.then(function (response) {
--- client/test/spec/drug-events/drug-events-controller_test.js
+++ client/test/spec/drug-events/drug-events-controller_test.js
@@ -24,7 +24,7 @@ describe('drug-events-controller', function () {
       deferred.resolve();
       return deferred.promise;
     },
-    calculateReactionOutcomes: function () {
+    searchIndications: function () {
       var deferred = $q.defer();
       deferred.resolve();
       return deferred.promise;
@@ -33,85 +33,96 @@ describe('drug-events-controller', function () {
   };
 
   it('searches using drugId and gender', function () {
-    spyOn(MockDrugEventsService, 'calculateReactionOutcomes').and.callThrough();
+    spyOn(MockDrugEventsService, 'searchEvents').and.callThrough();
     ctrl.drugId = 'Synthroid';
     ctrl.gender = 2;
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchResults[0].term).toBe('DISEASE');
-    expect(MockDrugEventsService.calculateReactionOutcomes).toHaveBeenCalledWith(
+    expect(MockDrugEventsService.searchEvents).toHaveBeenCalledWith(
       jasmine.objectContaining({
-        search: 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientsex:2'
+        drug: 'synthroid',
+        'patient.sex': 2
       }));
   });
 
   it('adds age range to search if it is set', function () {
-    spyOn(MockDrugEventsService, 'calculateReactionOutcomes').and.callThrough();
+    spyOn(MockDrugEventsService, 'searchEvents').and.callThrough();
     ctrl.drugId = 'Synthroid';
     ctrl.gender = 1;
     ctrl.age = 70;
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchResults[0].term).toBe('DISEASE');
-    expect(MockDrugEventsService.calculateReactionOutcomes).toHaveBeenCalledWith(
+    expect(MockDrugEventsService.searchEvents).toHaveBeenCalledWith(
       jasmine.objectContaining({
-        search: 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientsex:1+AND+patient.patientonsetage:[60+TO+80]'
+        drug: 'synthroid',
+        'patient.age.low': 60,
+        'patient.age.high': 80
       }));
   });
 
   it('does not allow the age range to go below 0', function () {
-    spyOn(MockDrugEventsService, 'calculateReactionOutcomes').and.callThrough();
+    spyOn(MockDrugEventsService, 'searchEvents').and.callThrough();
     ctrl.drugId = 'Synthroid';
     ctrl.gender = 1;
     ctrl.age = 5;
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchResults[0].term).toBe('DISEASE');
-    expect(MockDrugEventsService.calculateReactionOutcomes).toHaveBeenCalledWith(
+    expect(MockDrugEventsService.searchEvents).toHaveBeenCalledWith(
       jasmine.objectContaining({
-        search: 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientsex:1+AND+patient.patientonsetage:[0+TO+15]'
+        drug: 'synthroid',
+        'patient.age.low': 0,
+        'patient.age.high': 15
       }));
   });
 
   it('does not allow the age range to go above 120', function () {
-    spyOn(MockDrugEventsService, 'calculateReactionOutcomes').and.callThrough();
+    spyOn(MockDrugEventsService, 'searchEvents').and.callThrough();
     ctrl.drugId = 'Synthroid';
     ctrl.gender = 1;
     ctrl.age = 115;
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchResults[0].term).toBe('DISEASE');
-    expect(MockDrugEventsService.calculateReactionOutcomes).toHaveBeenCalledWith(
+    expect(MockDrugEventsService.searchEvents).toHaveBeenCalledWith(
       jasmine.objectContaining({
-        search: 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientsex:1+AND+patient.patientonsetage:[105+TO+120]'
+        drug: 'synthroid',
+        'patient.age.low': 105,
+        'patient.age.high': 120
       }));
   });
 
   it('adds weight range to search if it is set', function () {
-    spyOn(MockDrugEventsService, 'calculateReactionOutcomes').and.callThrough();
+    spyOn(MockDrugEventsService, 'searchEvents').and.callThrough();
     ctrl.drugId = 'Synthroid';
     ctrl.gender = 1;
     ctrl.weight = 180;
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchResults[0].term).toBe('DISEASE');
-    expect(MockDrugEventsService.calculateReactionOutcomes).toHaveBeenCalledWith(
+    expect(MockDrugEventsService.searchEvents).toHaveBeenCalledWith(
       jasmine.objectContaining({
-        search: 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientsex:1+AND+patient.patientweight:[170+TO+190]'
+        drug: 'synthroid',
+        'patient.weight.low': 170,
+        'patient.weight.high': 190
       }));
   });
 
   it('does not allow the weight range to go below 0', function () {
-    spyOn(MockDrugEventsService, 'calculateReactionOutcomes').and.callThrough();
+    spyOn(MockDrugEventsService, 'searchEvents').and.callThrough();
     ctrl.drugId = 'Synthroid';
     ctrl.gender = 1;
     ctrl.weight = 5;
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchResults[0].term).toBe('DISEASE');
-    expect(MockDrugEventsService.calculateReactionOutcomes).toHaveBeenCalledWith(
+    expect(MockDrugEventsService.searchEvents).toHaveBeenCalledWith(
       jasmine.objectContaining({
-        search: 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientsex:1+AND+patient.patientweight:[0+TO+15]'
+        drug: 'synthroid',
+        'patient.weight.low': 0,
+        'patient.weight.high': 15
       }));
   });
 
@@ -121,6 +132,7 @@ describe('drug-events-controller', function () {
       deferred.reject({status: 404});
       return deferred.promise;
     };
+    ctrl.drugId = 'Synthroid';
     ctrl.doSearch();
     $rootScope.$digest();
     expect(ctrl.searchAlerts[0].type).toBe('danger');
