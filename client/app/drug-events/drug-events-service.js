fae9369f694d7eb22d7debcbf8887bb779d4a355
brandony <brandony@part.net>
2015-07-01 16:56:37 -0600

    ads-proto-59: Implemented a strategy to retry requests that failed due to frequency/timing limitations. Cleaned up reaction outcome request function.

--- client/app/drug-events/drug-events-service.js
+++ client/app/drug-events/drug-events-service.js
@@ -34,56 +34,82 @@
         }
       };
 
+      /**
+       * @ngdoc function
+       * @author brandony-pn
+       *
+       * @description
+       * In an attempt to work with the limitations the openfda rest API, this recursive method was implemented to issue
+       * 'reaction outcome' requests sequentially, rather than concurrently. Requests still that fail due to server frequency
+       * constraints (429 status) will be re-tried up to 3 times.
+       *
+       * @param deferred the deferred promise from the reaction query
+       * @param outcomeQuery the query object that describes the details of this reaction/outcome request
+       * @param index the current reaction result index used to update the calculated object graph which is ultimately
+       * used for client display
+       */
       var doReactionOutcomeRequest = function (deferred, outcomeQuery, index) {
-        var thisQuery = angular.copy(outcomeQuery);
+        var retries = 0;
+        var calculateOutcomeQuery = function () {
+          var oc = angular.copy(outcomeQuery);
+          oc.search += '+AND+patient.reaction.reactionmeddrapt:"' + drugEventsFact.reactionResults[index].term + '"';
+          delete oc.limit;
+          return oc;
+        };
 
-        thisQuery.search += '+AND+patient.reaction.reactionmeddrapt:"' + drugEventsFact.reactionResults[index].term + '"';
-        delete thisQuery.limit;
+        var issueNext = function () {
+          if (++index < drugEventsFact.reactionResults.length) {
+            doReactionOutcomeRequest(deferred, outcomeQuery, index);
+          } else {
+            deferred.resolve();
+          }
+        };
 
-        DrugEvents.get(thisQuery).$promise.then(function (response) {
-          drugEventsFact.reactionResults[index].outcomes = response.results;
+        var buildUnknownResult = function () {
+          return {
+            term: 'Unknown',
+            count: drugEventsFact.reactionResults[index].count
+          };
+        };
 
-          var outcomes = 0;
-          Object.keys(response.results).map(function (key) {
-            outcomes += response.results[key].count;
-            response.results[key].term = convertOutcomeTerm(response.results[key].term);
-          });
+        var makeRequest = function (thisQuery) {
+          DrugEvents.get(thisQuery).$promise.then(function (response) {
+            drugEventsFact.reactionResults[index].outcomes = response.results;
 
-          if (outcomes < drugEventsFact.reactionResults[index].count) {
-            var unknown = drugEventsFact.reactionResults[index].outcomes.filter(function (value) {
-              return value.term.toUpperCase() === 'UNKNOWN';
+            var outcomes = 0;
+            Object.keys(response.results).map(function (key) {
+              outcomes += response.results[key].count;
+              response.results[key].term = convertOutcomeTerm(response.results[key].term);
             });
 
-            if (unknown.length === 1) {
-              unknown[0].count += drugEventsFact.reactionResults[index].count - outcomes;
-            } else {
-              drugEventsFact.reactionResults[index].outcomes.push({
-                term: 'Unknown',
-                count: drugEventsFact.reactionResults[index].count
+            if (outcomes < drugEventsFact.reactionResults[index].count) {
+              var unknown = drugEventsFact.reactionResults[index].outcomes.filter(function (value) {
+                return value.term.toUpperCase() === 'UNKNOWN';
               });
+
+              if (unknown.length === 1) {
+                unknown[0].count += drugEventsFact.reactionResults[index].count - outcomes;
+              } else {
+                drugEventsFact.reactionResults[index].outcomes.push(buildUnknownResult());
+              }
             }
-          }
-          index++;
-          if (index < drugEventsFact.reactionResults.length) {
-            doReactionOutcomeRequest(deferred, outcomeQuery, index);
-          } else {
-            deferred.resolve();
-          }
-        }, function (response) {
-          switch (response.status) {
-            case 404:
-              drugEventsFact.reactionResults[index].outcomes = [{
-                term: 'Unknown',
-                count: drugEventsFact.reactionResults[index].count
-              }];
-          }
-          index++;
-          if (index < drugEventsFact.reactionResults.length) {
-            doReactionOutcomeRequest(deferred, outcomeQuery, index);
-          } else {
-            deferred.resolve();
-          }
-        });
+
+            issueNext();
+          }, function (response) {
+            switch (response.status) {
+              case 404:
+                drugEventsFact.reactionResults[index].outcomes = [buildUnknownResult()];
+                issueNext();
+                break;
+              case 429:
+                if (retries++ < 3) {
+                  makeRequest(thisQuery);
+                }
+            }
+          });
+        };
+
+        makeRequest(calculateOutcomeQuery());
       };
 
       var serializer = function (params) {
@@ -139,10 +165,9 @@
       };
 
       drugEventsFact.calculateReactionOutcomes = function (outcomeQuery) {
-        var index = 0;
         var deferred = $q.defer();
 
-        doReactionOutcomeRequest(deferred, outcomeQuery, index);
+        doReactionOutcomeRequest(deferred, outcomeQuery, 0);
 
         return deferred.promise;
       };
