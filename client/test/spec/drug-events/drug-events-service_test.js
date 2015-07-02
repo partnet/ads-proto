9eb471cca589e17e1d2ed6955cd87551f6be0f68
brandony <brandony@part.net>
2015-07-02 09:17:33 -0600

    Fix unit tests after changing the pool1 serializer.

--- client/test/spec/drug-events/drug-events-service_test.js
+++ client/test/spec/drug-events/drug-events-service_test.js
@@ -15,8 +15,8 @@ describe('drug-events-service', function () {
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('serializes parameters without encoding +', function () {
-    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:Synthroid+AND+patient.patientweight:[180+TO+200]&count=patient.reaction.reactionmeddrapt.exact&limit=20').respond();
+  it('serializes parameters without encoding "+"', function () {
+    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name%3ASynthroid+AND+patient.patientweight%3A%5B180+TO+200%5D&count=patient.reaction.reactionmeddrapt.exact&limit=20').respond();
     var params = {'search': 'patient.drug.openfda.brand_name:Synthroid+AND+patient.patientweight:[180+TO+200]',
                   'count': 'patient.reaction.reactionmeddrapt.exact',
                   'limit': '20'};
@@ -28,8 +28,8 @@ describe('drug-events-service', function () {
     var params = {'search': 'patient.drug.openfda.brand_name:Synthroid',
                   'count': 'patient.reaction.reactionmeddrapt.exact',
                   'limit': '20'};
-    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:Synthroid+AND+patient.reaction.reactionmeddrapt:"NAUSEA"&count=patient.reaction.reactionmeddrapt.exact').respond({results: {'NAUSEA': {term: 5, count: 83}}});
-    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:Synthroid+AND+patient.reaction.reactionmeddrapt:"DYSPNOEA"&count=patient.reaction.reactionmeddrapt.exact').respond({results: {'DYSPNOEA': {term: 6, count: 72}}});
+    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name%3ASynthroid+AND+patient.reaction.reactionmeddrapt%3A%22NAUSEA%22&count=patient.reaction.reactionmeddrapt.exact').respond({results: {'NAUSEA': {term: 5, count: 83}}});
+    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name%3ASynthroid+AND+patient.reaction.reactionmeddrapt%3A%22DYSPNOEA%22&count=patient.reaction.reactionmeddrapt.exact').respond({results: {'DYSPNOEA': {term: 6, count: 72}}});
     DrugEventsService.reactionResults = [{term: 'NAUSEA', count: 20}, {term: 'DYSPNOEA', count: 15}];
     DrugEventsService.calculateReactionOutcomes(params);
     $httpBackend.flush();
@@ -40,11 +40,11 @@ describe('drug-events-service', function () {
     expect(DrugEventsService.reactionResults[1].outcomes.DYSPNOEA.count).toBe(72);
   });
 
-  it('sets outcome to 6 when a 404 is returned', function () {
+  it('sets outcome to "Unknown" when a 404 is returned', function () {
     var params = {'search': 'patient.drug.openfda.brand_name:Synthroid',
                   'count': 'patient.reaction.reactionmeddrapt.exact',
                   'limit': '20'};
-    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:Synthroid+AND+patient.reaction.reactionmeddrapt:"NAUSEA"&count=patient.reaction.reactionmeddrapt.exact').respond(404, '');
+    $httpBackend.expectGET('https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name%3ASynthroid+AND+patient.reaction.reactionmeddrapt%3A%22NAUSEA%22&count=patient.reaction.reactionmeddrapt.exact').respond(404, '');
     DrugEventsService.reactionResults = [{term: 'NAUSEA', count: 20}];
     DrugEventsService.calculateReactionOutcomes(params);
     $httpBackend.flush();
